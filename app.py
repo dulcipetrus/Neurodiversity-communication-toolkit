9bb3f1fdcdc736abb522fe66c2966fb2ca95dd50
Chawalaa <chawala.banda@keio.jp>
2025-12-24 13:14:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 # -------------------------------------------------
-# THEME (Dark Blue / Light Blue)
+# THEME (Pastel)
 # -------------------------------------------------
 st.markdown(
     """
